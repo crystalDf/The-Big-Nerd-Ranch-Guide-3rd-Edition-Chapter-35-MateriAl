1261347ea2cf4da8978f85980ad3dbd9e221f52f
crystalDf <chendong333@gmail.com>
2018-01-21 00:10:53 +0800

    Chapter 35 MateriAl

---
+++ app/src/main/java/com/star/material/MainActivity.java
@@ -0,0 +1,24 @@
+package com.star.material;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final View container = findViewById(R.id.container);
+
+        container.setOnClickListener(v -> Snackbar.make(container, R.string.munch, Snackbar.LENGTH_SHORT).show());
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+    }
+}
